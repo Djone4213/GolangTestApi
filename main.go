42d5a97886a7751242785fbf5c6dcbd89f7414a7
Djone <djone_42@mail.ru>
2022-06-05 14:49:08 +0700

    Доработка, реализация получения необходимых скилов по выбраной вакансии

--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"net/http"
-	// "log"
 	"encoding/json"
-	 "fmt"
+	"fmt"
 )
 
 type tItem struct{
@@ -16,34 +15,105 @@ type tResponse struct{
 	Items []tItem 
 }
 
-const cUrl = "https://api.hh.ru/vacancies"
-
-func main() {
-	makeRequest()
+type tItemsInfo struct {
+	Name string
 }
 
-func makeRequest() {
+type tResponseItemInfo struct {
+	Key_skills []tItemsInfo
+}
 
+const cUrl = "https://api.hh.ru/vacancies"
+
+func getReqestData(urlAPI string, result interface{}) (error) {
 	client := &http.Client{} 
-    req, err := http.NewRequest("GET", cUrl, nil, 
+    req, err := http.NewRequest("GET", urlAPI, nil, 
     ) 
+	if err != nil {
+		return err
+	}
     // добавляем заголовки
     req.Header.Add("User-Agent", "api-test-agent") 
   
     resp, err := client.Do(req) 
     if err != nil { 
-        fmt.Println(err) 
-        return
+        return err
     } 
     defer resp.Body.Close() 
-
-	var result tResponse
 	
 	json.NewDecoder(resp.Body).Decode(&result)
+	return nil
+}
 
-	for key, val := range result.Items {
-		fmt.Printf("%d %s\n",key + 1, val.Name)
+func getListVacancies() ([]tItem, error) {
+	var responce tResponse
+
+	err := getReqestData(cUrl, &responce)
+	if err != nil {
+		return []tItem{}, err
 	}
+	return responce.Items, nil
+}
+
+func getSkillsVacancie(id string) (string, error) {
+	var itemInfo tResponseItemInfo
+
+	err := getReqestData(cUrl + "/" + id, &itemInfo)
+	if err != nil {
+		return "", err
+	}
+
+	var result string
+
+	for _, val := range itemInfo.Key_skills {
+		result += val.Name + ","
+	}
+	
+	if result != "" {
+		result = result[:len(result) - 1]
+	}
+
+	return result, nil
+}
+
+func main() {
+	listVacancies, err := getListVacancies();
+	var a int
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		for key, val := range listVacancies {
+			fmt.Printf("%d %s\n",key + 1, val.Name)
+		}
+
+		fmt.Print("Введите номер вакансии для просмотра необходимых скилов, для выхода введите 0\n")
+		fmt.Scan(&a)
+
+		if (a == 0) {
+			break
+		} else if (a > len(listVacancies)) {
+			fmt.Print("Введен не корректный номер вакансии\n")	
+		} else {
+			skillInfo, err := getSkillsVacancie(listVacancies[a - 1].Id)
+			if err != nil {
+				fmt.Printf("Ошибка при получении информаиции о скалах\n%s", err)
+			}
+
+			if skillInfo != "" {
+				fmt.Printf("Перечень скилов:%s\n",skillInfo)
+			} else {
+				fmt.Printf("Для выбранной вакансии перечень скилов не задан\n")
+			}
+			
+			break
+		}
+
+	}
+	
+
 	
 }
 
